d59beb757c2d209cd66de810607b24ea6ef13a70
Eseza Muwanga Najjombwe <43315519+titan256@users.noreply.github.com>
2023-07-23 13:03:50 +0300

    Removing ERP logic from tax service and transferring to app

--- backend/app/api/routes/__init__.py
+++ backend/app/api/routes/__init__.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from app.api.routes.taxes import router as taxes_router
-from app.api.routes.bitrix import router as bitrix_router
-from app.api.routes.dear import router as dear_router
+
 
 from app.api.routes.common import router as common_router
 
@@ -10,16 +9,6 @@ router = APIRouter()
 router.include_router(taxes_router, prefix="/taxmoja", tags=["Taxmoja"],
                       responses={404: {"description": "oops can't help "
                                                       "you with that!"}})
-
-router.include_router(bitrix_router, prefix='/bitrix24',
-                      tags=['Bitrix24'],
-                      responses={404: {"description": "oops can't help you with that!"}}, )
-
-
-
-router.include_router(dear_router, prefix='/dear',
-                      tags=['Dear'],
-                      responses={404: {"description": "oops can't help you with that!"}}, )
 
 router.include_router(common_router, prefix="", tags=["Paymoja"],
                       responses={404: {"description": "oops can't help "
